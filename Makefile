1aecba7a66b1489333ecb3e6749f14b6c293a48d
Atharva Lele <itsatharva@gmail.com>
2020-05-26 11:54:22 +0530

    Initial Makefile and sources list
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

---
+++ Makefile
@@ -0,0 +1,115 @@
+#################################################################
+# GCC + Makefile based build system for STM32L476-DISCOVERY Board
+# Based on configuration generated using STM32CubeMX
+# Supports HAL + LL Libraries
+# Copyright (C) 2020 Atharva Lele
+#################################################################
+
+#######################################
+# makefile includes
+#######################################
+include sources.mk
+
+# target
+TARGET = project_name
+
+# debug? default yes
+DEBUG = 1
+
+# Build in a separate directory to keep cleanup easy
+BUILD_DIR = build
+
+# gcc binaries
+PREFIX = arm-none-eabi-
+CC = $(PREFIX)gcc
+AS = $(PREFIX)gcc -x assembler-with-cpp
+CP = $(PREFIX)objcopy
+SZ = $(PREFIX)size
+HEX = $(CP) -O ihex
+BIN = $(CP) -O binary -S
+
+#######################################
+# Microcontroller options
+#######################################
+CPU = -mcpu=cortex-m4
+FPU = -mfpu=fpv4-sp-d16
+FLOAT_ABI = -mfloat-abi=hard
+# overall config therefore is
+MCU = $(CPU) -mthumb $(FPU) $(FLOAT-ABI)
+
+#######################################
+# GCC Setup
+#######################################
+# Assembly defines
+AS_DEFS = 
+
+# C Defines
+C_DEFS = -DSTM32L476xx \
+		 -DUSE_HAL_DRIVER
+
+# compile GCC flags
+ASFLAGS = $(MCU) $(AS_DEFS) $(AS_INCLUDES) $(OPT) -Wall -fdata-sections -ffunction-sections
+CFLAGS = $(MCU) $(C_DEFS) $(C_INCLUDES) $(OPT) -Wall -fdata-sections -ffunction-sections
+
+# Debug and optimization
+ifeq ($(DEBUG), 1)
+# enable debug, optimize for debugging
+CFLAGS += -g -gdwarf-2 -Og
+else
+# optimize for code size
+CFLAGS += -Os
+endif
+
+#######################################
+# Linker Setup
+#######################################
+LDSCRIPT = STM32L476VGTx_FLASH.ld
+
+# libraries
+LIBS = -lc -lm -lnosys 
+LIBDIR = 
+LDFLAGS = $(MCU) -specs=nano.specs -T$(LDSCRIPT) $(LIBDIR) $(LIBS) \
+		  -Wl,-Map=$(BUILD_DIR)/$(TARGET).map,--cref -Wl,--gc-sections
+
+#######################################
+# Build
+#######################################
+# list of objects
+OBJECTS = $(addprefix $(BUILD_DIR)/,$(notdir $(C_SOURCES:.c=.o)))
+vpath %.c $(sort $(dir $(C_SOURCES)))
+# list of ASM program objects
+OBJECTS += $(addprefix $(BUILD_DIR)/,$(notdir $(ASM_SOURCES:.s=.o)))
+vpath %.s $(sort $(dir $(ASM_SOURCES)))
+
+$(BUILD_DIR)/%.o: %.c Makefile | $(BUILD_DIR) 
+	$(CC) -c $(CFLAGS) -Wa,-a,-ad,-alms=$(BUILD_DIR)/$(notdir $(<:.c=.lst)) $< -o $@
+
+$(BUILD_DIR)/%.o: %.s Makefile | $(BUILD_DIR)
+	$(AS) -c $(CFLAGS) $< -o $@
+
+$(BUILD_DIR)/$(TARGET).elf: $(OBJECTS) Makefile
+	$(CC) $(OBJECTS) $(LDFLAGS) -o $@
+	$(SZ) $@
+
+$(BUILD_DIR)/%.hex: $(BUILD_DIR)/%.elf | $(BUILD_DIR)
+	$(HEX) $< $@
+	
+$(BUILD_DIR)/%.bin: $(BUILD_DIR)/%.elf | $(BUILD_DIR)
+	$(BIN) $< $@	
+	
+$(BUILD_DIR):
+	mkdir $@
+
+# default action: build all
+all: $(BUILD_DIR)/$(TARGET).elf $(BUILD_DIR)/$(TARGET).hex $(BUILD_DIR)/$(TARGET).bin
+
+#######################################
+# Dependencies
+#######################################
+-include $(wildcard $(BUILD_DIR)/*.d)
+
+#######################################
+# Cleanup
+#######################################
+clean:
+	-rm -rf $(BUILD_DIR)
---
+++ sources.mk
@@ -0,0 +1,66 @@
+#################################################################
+# GCC + Makefile based build system for STM32L476-DISCOVERY Board
+# Based on configuration generated using STM32CubeMX
+# Supports HAL + LL Libraries
+# Copyright (C) 2020 Atharva Lele
+#################################################################
+
+# Put your sources and includes in this file
+
+# C includes
+C_INCLUDES = -IInc \
+			 -IDrivers/STM32L4xx_HAL_Driver/Inc \
+			 -IDrivers/STM32L4xx_HAL_Driver/Inc/Legacy \
+			 -IDrivers/CMSIS/Device/ST/STM32L4xx/Include \
+			 -IDrivers/CMSIS/Include
+
+# ASM Sources
+ASM_SOURCES = startup_stm32l476xx.s
+
+# C Sources
+C_SOURCES = Src/main.c \
+			Src/system_stm32l4xx.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_adc.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_comp.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_crc.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_crs.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_dac.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_dma2d.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_dma.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_exti.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_fmc.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_gpio.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_i2c.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_lptim.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_lpuart.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_opamp.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_pka.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_pwr.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_rcc.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_rng.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_rtc.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_sdmmc.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_spi.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_swpmi.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_tim.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_usart.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_usb.c \
+			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_utils.c
+			 
