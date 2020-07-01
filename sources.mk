b54110e27387dbc002cdbc3579b847e0a8277d93
Atharva Lele <itsatharva@gmail.com>
2020-06-30 22:26:17 -0700

    sources.mk: drop compilation of HAL files since we're not using them..
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

--- sources.mk
+++ sources.mk
@@ -20,23 +20,6 @@ ASM_SOURCES = startup_stm32l476xx.s
 # C Sources
 C_SOURCES = Src/main.c \
 			Src/system_stm32l4xx.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c \
-			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c \
 			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_adc.c \
 			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_comp.c \
 			Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_crc.c \
